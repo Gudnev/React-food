4f19c5bf40563fb2815bf380f37f8363d7af0189
Nikolay Gudnev <gudnev22@gmail.com>
2024-04-21 18:29:38 +0500

    react-food

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
-  return (
-    <nav className="green darken-1">
-      <div className="nav-wrapper">
-        <a href="/" className="brand-logo">
-          React Shop
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <a
-              href="https://github.com/Gudnev/React-shop"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Repo
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className='green darken-1'>
+            <div className='nav-wrapper'>
+                <Link to='/' className='brand-logo'>
+                    React Food
+                </Link>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <Link to={`/about`}>About</Link>
+                    </li>
+                    <li>
+                        <Link to={`/contacts`}>Contacts</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export { Header };
